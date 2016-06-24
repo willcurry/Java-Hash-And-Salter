aa96187b99ce8fb7dbd02153fd928d59e60dec88
willcurry <willcurry@Wills-MBP.default>
2016-06-24 14:58:39 +0100

    Remove spaces, remove variables being returned and just returned the actual value and refactored tests.

--- src/HashAndSaltTests.java
+++ src/HashAndSaltTests.java
@@ -1,18 +1,49 @@
+import com.sun.codemodel.internal.ClassType;
+import com.sun.tools.classfile.Type;
 import org.junit.Test;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertEquals;
 
 public class HashAndSaltTests {
-    @Test
-    public void returnsString() {
-        HashAndSalter hashAndSalter = new HashAndSalter();
-        hashAndSalter.salt();
+
+    public static String hash(String pw) {
+        MessageDigest m = null;
+        try {
+            m = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        m.reset();
+        m.update(pw.getBytes());
+        BigInteger bigInt = new BigInteger(1, m.digest());
+        String hashtext = bigInt.toString(16);
+
+        while(hashtext.length() < 32 ){
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
     }
 
     @Test
     public void checkPasswordFunction() {
         HashAndSalter hashAndSalter = new HashAndSalter();
-        hashAndSalter.enteredPassword("test");
+        assertEquals("".getClass(), hashAndSalter.enteredPassword("test").getClass());
     }
 
+    @Test
+    public void checkSalt() {
+        HashAndSalter hashAndSalter = new HashAndSalter();
+        assertEquals(true, hashAndSalter.salt().contains("salted"));
+    }
+
+    @Test
+    public void checkHash() {
+        HashAndSalter hashAndSalter = new HashAndSalter();
+        assertEquals(hash("test"), hashAndSalter.hash("test"));
+    }
 }
+
--- src/HashAndSalter.java
+++ src/HashAndSalter.java
@@ -7,7 +7,6 @@ import java.security.*;
 public class HashAndSalter {
 
     public static String hash(String pw) {
-
         MessageDigest m = null;
         try {
             m = MessageDigest.getInstance("MD5");
@@ -16,47 +15,33 @@ public class HashAndSalter {
         }
         m.reset();
         m.update(pw.getBytes());
-        byte[] digest = m.digest();
-        BigInteger bigInt = new BigInteger(1, digest);
+        BigInteger bigInt = new BigInteger(1, m.digest());
         String hashtext = bigInt.toString(16);
 
         while(hashtext.length() < 32 ){
-            hashtext = "0"+hashtext;
+            hashtext = "0" + hashtext;
         }
-
         return hashtext;
     }
 
     public static String salt() {
         String[] characters = {"dj3", "jeii2", "2374", "xn32974", "3883", "duui3", "ccc", "lkoi", "2923", "mcnwiud", "1sd1f"};
-
         String saltedText = "";
-
         for (int i = 0; i < 10; i++) {
             int index = randomPick(characters);
             saltedText += characters[index];
         }
-
+        saltedText += "salted";
         return saltedText;
     }
 
 
     public static int randomPick(String[] characters) {
         Random rand = new Random();
-
-        int index = rand.nextInt(characters.length);
-
-        return index;
+        return rand.nextInt(characters.length);
     }
 
     public static String enteredPassword(String pw) {
-        String password = pw;
-
-        String hashSaltedPassword = hash(pw + salt());
-
-        return hashSaltedPassword;
-
+        return hash(pw + salt());
     }
-
-
 }
